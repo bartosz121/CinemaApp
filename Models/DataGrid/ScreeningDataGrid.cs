f4bf45c26494d3a4e7839e7b0b447d79b244fc02
bartosz121 <bmagiera121@gmail.com>
2022-05-21 14:21:31 +0200

    Add ScreeningDataGrid model

---
+++ Models/DataGrid/ScreeningDataGrid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Models.DataGrid
+{
+    public class ScreeningDataGrid
+    {
+        public ScreeningDataGrid(int screeningId, string movieTitle, string roomName, double price, DateTime date, int ticketsLeft)
+        {
+            ScreeningId = screeningId;
+            MovieTitle = movieTitle;
+            RoomName = roomName;
+            Price = price;
+            Date = date;
+            TicketsLeft = ticketsLeft;
+        }
+
+        public int ScreeningId { get; set; }
+
+        public string MovieTitle { get; set; }
+
+        public string RoomName { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int TicketsLeft { get; set; }
+
+    }
+}
