8d7c6c0797d3cd3daff0985895a24f27f04455a4
bartosz121 <bmagiera121@gmail.com>
2022-05-21 12:51:05 +0200

    Add dbContext

---
+++ Context/dbContext.cs
@@ -0,0 +1,24 @@
+﻿using CinemaApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Context
+{
+    public class dbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = database.db");
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Screening> Screenings { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
