5029b7b406dc7ae53f8957eadfc6b2d375702a02
bartosz121 <bmagiera121@gmail.com>
2022-05-21 12:47:32 +0200

    Add Movie model

---
+++ Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public virtual List<Screening> Screenings { get; set; }
+    }
+}
