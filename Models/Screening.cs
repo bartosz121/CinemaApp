b3e29010f0ca8c51e0b4bdf383e9ecb23385bb9e
bartosz121 <bmagiera121@gmail.com>
2022-05-21 12:48:01 +0200

    Add Screening model

---
+++ Models/Screening.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Models
+{
+    public class Screening
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int RoomId { get; set; }
+        public int MovieId { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public virtual List<Ticket> Tickets { get; set; }
+
+        public virtual Room Room { get; set; }
+        public virtual Movie Movie { get; set; }
+
+    }
+}
