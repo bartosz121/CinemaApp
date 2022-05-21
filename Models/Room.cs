d0d21fa3a363057d8a50482c2969989d012af780
bartosz121 <bmagiera121@gmail.com>
2022-05-21 12:47:45 +0200

    Add Room model

---
+++ Models/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Models
+{
+    public class Room
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public virtual List<Screening> Screenings { get; set; }
+    }
+}
