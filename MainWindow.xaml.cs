39b497fcefe0835a9703c0bb6b3f6f1444dfc522
bartosz121 <bmagiera121@gmail.com>
2022-05-21 13:18:37 +0200

    Add MainWindow

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CinemaApp.Context;
+using CinemaApp.Models;
+using CinemaApp.Pages;
+using CinemaApp.Pages.UserPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CinemaApp
+{
+    public partial class MainWindow : Window
+    {
+        private dbContext context;
+        protected User user { get; set; }
+        public MainWindow(User user)
+        {
+            InitializeComponent();
+            this.context = new dbContext();
+            this.user = user;
+            //MainFrame.Content = new SignInPage(context);
+        }
+
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btn1Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new Page1();
+            this.MenuToggleButton.IsChecked = false;
+        }
+    }
+}
