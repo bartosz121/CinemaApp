261b1d89725c698c7ce144cdf0b5948b102c3496
bartosz121 <bmagiera121@gmail.com>
2022-05-21 12:48:13 +0200

    Add User model

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        [DefaultValue(false)]
+        public bool isAdmin { get; set; }
+
+        public virtual List<Ticket> Tickets { get; set; }
+    }
+}
