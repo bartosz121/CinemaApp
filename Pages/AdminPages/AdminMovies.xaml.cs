4dad85e34a11c8c0a32e181e919082a7ae32b301
bartosz121 <bmagiera121@gmail.com>
2022-05-21 14:20:38 +0200

    Add AdminMovies page

---
+++ Pages/AdminPages/AdminMovies.xaml.cs
@@ -0,0 +1,126 @@
+﻿using CinemaApp.Context;
+using CinemaApp.Models;
+using CinemaApp.Models.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CinemaApp.Pages.AdminPages
+{
+    /// <summary>
+    /// Interaction logic for AdminMovies.xaml
+    /// </summary>
+    public partial class AdminMovies : Page
+    {
+        private dbContext context;
+        private Movie? selectedMovie;
+        public AdminMovies()
+        {
+            InitializeComponent();
+            context = new dbContext();
+            dataGrid.ItemsSource = context.Movies.ToList();
+        }
+
+        private void refreshDataGrid()
+        {
+            dataGrid.ItemsSource = context.Movies.ToList();
+        }
+
+        private void displaySelectedMovieData()
+        {
+            if (selectedMovie != null)
+            {
+                txtTitle.Text = selectedMovie.Title;
+                txtYear.Text = selectedMovie.Year.ToString();
+            }
+        }
+
+        private void dataGridSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (DataGrid)sender;
+            var movie = (Movie)item.SelectedItem;
+
+            selectedMovie = movie;
+            displaySelectedMovieData();
+        }
+
+        private void btnClickAdd(object sender, RoutedEventArgs e)
+        {
+            MovieFormData data;
+            try
+            {
+                data = getFormData();
+                Movie movie = new Movie { Title = data.Title, Year = data.Year };
+                context.Movies.Add(movie);
+
+                context.SaveChanges();
+                refreshDataGrid();
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnClickUpdate(object sender, RoutedEventArgs e)
+        {
+            if (selectedMovie != null)
+            {
+                MovieFormData data;
+                try
+                {
+                    data = getFormData();
+                    selectedMovie.Title = data.Title;
+                    selectedMovie.Year = data.Year;
+
+                    context.SaveChanges();
+                    refreshDataGrid();
+                }
+                catch (ArgumentException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btnClickDelete(object sender, RoutedEventArgs e)
+        {
+            if (selectedMovie != null){ 
+                context.Movies.Remove(selectedMovie);
+                context.SaveChanges();
+                selectedMovie = null;
+                refreshDataGrid();
+            }
+
+        }
+
+        private MovieFormData getFormData()
+        {
+            string title = txtTitle.Text;
+
+            int year;
+            int time;
+            MovieFormData data;
+
+            try
+            {
+                year = int.Parse(txtYear.Text);
+                data = new MovieFormData(title, year);
+                return data;
+            } catch
+            {
+                throw new ArgumentException("Invalid data");
+            }
+        }
+    }
+}
