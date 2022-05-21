067719989f3a95535445935d6dcb7c0e7e3da8bb
bartosz121 <bmagiera121@gmail.com>
2022-05-21 15:07:48 +0200

    Add MovieFormData model

---
+++ Models/Forms/MovieFormData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Models.Forms
+{
+    public class MovieFormData
+    {
+        public MovieFormData(string title, int year)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException();
+            }
+            Title = title;
+            Year = year;
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+    }
+}
