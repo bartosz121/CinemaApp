65d0bf346c14b0267daa6ee68898fa4b7949f81d
bartosz121 <bmagiera121@gmail.com>
2022-05-21 14:21:17 +0200

    Add AdminUsers page

---
+++ Pages/AdminPages/AdminUsers.xaml.cs
@@ -0,0 +1,63 @@
+﻿using CinemaApp.Context;
+using CinemaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CinemaApp.Pages.AdminPages
+{
+    /// <summary>
+    /// Interaction logic for AdminUsers.xaml
+    /// </summary>
+    public partial class AdminUsers : Page
+    {
+        private dbContext context;
+        private User? selectedUser;
+        public AdminUsers()
+        {
+            InitializeComponent();
+            context = new dbContext();
+
+            populateUsersDataGrid();
+        }
+
+        private void populateUsersDataGrid()
+        {
+            dataGrid.ItemsSource = context.Users.ToList();
+        }
+
+        private void dataGridSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (DataGrid)sender;
+            var user = (User)item.SelectedItem;
+
+            if (user != null)
+            {
+                selectedUser = context.Users.Where(u => u.Id == user.Id).First();
+            }
+        }
+
+        private void btnClickDelete(object sender, RoutedEventArgs e)
+        {
+            if (selectedUser != null)
+            {
+                context.Users.Remove(selectedUser);
+                context.SaveChanges();
+                selectedUser = null;
+
+                populateUsersDataGrid();
+            }
+        }
+    }
+}
